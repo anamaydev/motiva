ad8bd6221962ad1b894006f9d40c572b9c7b8962
anamaydev <anamaydev@gmail.com>
2025-03-26 21:15:47 +0530

    feat: add quote navigation feature

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const body = document.getElementById('body');
+const quotePlaceholder = document.getElementById('quote-placeholder');
+const authorPlaceholder = document.getElementById('author-placeholder');
 const previousBtn = document.getElementById('previous-btn');
 const nextBtn = document.getElementById('next-btn');
 const screenshotBtn = document.getElementById('screenshot-btn');
@@ -7,13 +9,69 @@ const modeBtn = document.getElementById('mode-btn');
 const svgs = document.querySelectorAll('.svg');
 const moonIcon = document.getElementById('moon-icon');
 const sunIcon = document.getElementById('sun-icon');
+const URL = 'https://raw.githubusercontent.com/anamaydev/temp-quotes-api/refs/heads/main/quotes.json';
+let index = 0;
+let dataLength = 0;
 let isLight = true;
+
+// generating random index to select quote
+function generateRandomIndex(dataLength){
+  index =  Math.floor(Math.random()*dataLength);
+  console.log(`random index: ${index}`);
+}
+
+// inserting selected data
+function showData(index, data){
+  const quote = data[index];
+  quotePlaceholder.innerText = quote['text'];
+  authorPlaceholder.innerText = `- ${quote['author']}`;
+}
+
+function getNextData(dataLength, data){
+  index = (index + 1) % dataLength;
+  console.log(`next index: ${index}`);
+  showData(index, data);
+}
+
+function getPreviousData(dataLength, data){
+  index = (index - 1 + dataLength) % dataLength;
+  console.log(`previous index: ${index}`);
+  showData(index, data);
+}
+
+function setEventListeners(dataLength, data){
+  nextBtn.addEventListener('click', ()=>{
+    getNextData(dataLength, data);
+  });
+  previousBtn.addEventListener('click', ()=>{
+    getPreviousData(dataLength, data);
+  });
+}
+
+// getting data
+async function getData(){
+  const response = await fetch(URL);
+  const data = await response.json();
+  dataLength = data.length;
+  console.log(data);
+
+  // calling for random index
+  await generateRandomIndex(dataLength);
+  // calling to insert selected data
+  await showData(index, data);
+
+  setEventListeners(dataLength, data);
+}
+
+window.addEventListener('load', getData);
 
 modeBtn.addEventListener('click', ()=>{
   if(isLight){
+    // changing theme to dark
     body.classList.add('bg-(color:--clr-black)');
     body.classList.remove('bg-(color:--clr-white)');
 
+    // changing font color to light
     body.classList.add('text-(color:--clr-white)');
     body.classList.remove('text-(color:--clr-black)');
 
@@ -38,9 +96,11 @@ modeBtn.addEventListener('click', ()=>{
     modeText.innerText = 'light';
 
   }else{
+    // changing font color to light
     body.classList.add('bg-(color:--clr-white)');
     body.classList.remove('bg-(color:--clr-black)');
 
+    // changing font color to dark
     body.classList.add('text-(color:--clr-black)');
     body.classList.remove('text-(color:--clr-white)');
 
